cdbdd570984decabed7ab7cbb2bfcedced4dd52a
gottomy2 <igottomy@gmail.com>
2022-01-06 17:52:10 +0100

    adding game ending condition

---
+++ Assets/Scripts/ShipInterior/GameEnd.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class GameEnd : MonoBehaviour
+{
+    public GlobalVars global;
+    public MapData mapData;
+
+    private void Update()
+    {
+        if(!isEmpty(mapData.path))
+        {
+            if (!mapData.firstStart && !global.getVar("minigameFailed", global.hubStats) && mapData.path[mapData.path.Count - 1].Equals("Pend"))
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                SceneManager.LoadScene("Assets/Scenes/Fillers/FillerZF.unity");
+            }
+        }
+    }
+
+    private static bool isEmpty<T>(List<T> list)
+    {
+        if (list == null)
+        {
+            return true;
+        }
+        return !list.Any();
+    }
+}
