9ec24947bf5f0c6652322a11ec8ae93f8d54dc8b
gottomy2 <igottomy@gmail.com>
2022-01-10 23:24:49 +0100

    Fixing MapActive parameter
    Map should be active when:
    - assistant is destroyed for the first time,
    - player had talked to the assistant before
    - there are not repairs to be made.

--- Assets/Scripts/ShipInterior/Repairs.cs
+++ Assets/Scripts/ShipInterior/Repairs.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Repairs : MonoBehaviour
 {
-    public GlobalVars global;
-
     // Update is called once per frame
     void Update()
     {
         if (GlobalData.GetVar("minigameFailed", GlobalData.hubStats) && !GlobalData.GetVar("simonBroken", GlobalData.hubStats) && !GlobalData.GetVar("wiresBroken", GlobalData.hubStats) && !GlobalData.GetVar("switchesBroken", GlobalData.hubStats))
         {
-            GlobalData.SetVar("minigameFailed", false, GlobalData.hubStats);
+            Debug.Log("All repairs are made so the map is now Active and minigameFailed = false");
             GlobalData.SetVar("mapActive", true, GlobalData.hubStats);
+            GlobalData.SetVar("minigameFailed", false, GlobalData.hubStats);
         }
     }
 }
